b6e744632c6bd58cf2be595ea52c33a4fcd6fa8b
Carley Tripp <carley.tripp@outlook.com>
2020-03-29 23:50:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function addBreedsToDOM(json) {
 function doChallengeThree() {
   const breeds = document.getElementsByTagName('li');
 
-  for (const breed of breeds) {
-    console.log(breed);
-  }
+  breeds.addEventListener('click', () => {
+    breeds.style.color = 'green';
+  });
 }
