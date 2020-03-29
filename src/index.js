bf1e36208aeb4b2c69d5710db034bd708b99c5e8
Carley Tripp <carley.tripp@outlook.com>
2020-03-29 23:00:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
 
 document.addEventListener('DOMContentLoaded', (event) => {
   doChallengeOne();
 });
 
 function doChallengeOne() {
-  const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
-
   fetch(imgUrl)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
+      addImagesToDOM(json);
     });
 }
