5e3de716f6356550b3ca9f15da9e5873e7d6a357
Carley Tripp <carley.tripp@outlook.com>
2020-03-30 02:19:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function addBreedsToDOM(json) {
 
 function doChallengeThree() {
   const liElements = document.getElementsByTagName('li');
-  for (const li of liElements) {
+  for (const li in liElements) {
     console.log(liElements);
     // li.addEventListener('click', function(event) {
     //   event.target.style.color = 'green';
