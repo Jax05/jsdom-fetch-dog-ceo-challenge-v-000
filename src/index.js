056be5de7d6f304a286965ef32376c4fa26a8118
Carley Tripp <carley.tripp@outlook.com>
2020-03-30 00:39:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -63,5 +63,5 @@ function doChallengeThree() {
 
 function doChallengeFour() {
   const breedFilter = document.getElementById('breed-dropdown');
-  console.log(breedFilter.options);
+  console.log(breedFilter.selectedOptions);
 }
